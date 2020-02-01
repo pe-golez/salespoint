3352130a7f8bab5e5784f11b9fede518b508e462
pgolez <paul.golez@codingavenue.com>
2020-02-01 14:35:30 +0800

    Create new entity Item

---
+++ src/Entity/Item.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ItemRepository")
+ */
+class Item
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $itemNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $unitPrice;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItemNumber(): ?string
+    {
+        return $this->itemNumber;
+    }
+
+    public function setItemNumber(string $itemNumber): self
+    {
+        $this->itemNumber = $itemNumber;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUnitPrice(): ?float
+    {
+        return $this->unitPrice;
+    }
+
+    public function setUnitPrice(float $unitPrice): self
+    {
+        $this->unitPrice = $unitPrice;
+
+        return $this;
+    }
+}
