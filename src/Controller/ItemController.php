ac7b61edb9abe08347ee8871046e3a7dc8566f27
pgolez <paul.golez@codingavenue.com>
2020-02-01 17:54:21 +0800

    Database setup for items entity

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Item;
+use App\Repository\ItemRepository;
 
 class ItemController extends AbstractController
 {
+
+    public function __construct(ItemRepository $ir)
+    {
+        $this->itemRepository = $ir;
+    }
+
     /**
      * @Route("/item", name="item")
      */
     public function index()
     {
         $item1 = new Item();
-        $item1->setItemNumber('10120');
+        $item1->setItemNumber('xxx--xwfda');
         $item1->setDescription('pen');
         $item1->setUnitPrice(12.00);
 
@@ -28,28 +35,12 @@ class ItemController extends AbstractController
      */
     public function listItems()
     {
-        $item1 = new Item();
-        $item1->setItemNumber('10120');
-        $item1->setDescription('pen');
-        $item1->setUnitPrice(12.00);
+        // $items = $this->itemRepository->findBy([
+        //     'unitPrice' => 235,
+        //     'description'   => 'Mojitos Gold'
+        // ]);
+        $items = $this->itemRepository->findAll();
 
-        $item2 = new Item();
-        $item2->setItemNumber('101334');
-        $item2->setDescription('pen 2');
-        $item2->setUnitPrice(12.50);
-
-        $item3 = new Item();
-        $item3->setItemNumber('324234');
-        $item3->setDescription('expensive pen');
-        $item3->setUnitPrice(82.00);
-
-        $item4 = new Item();
-        $item4->setItemNumber('123123');
-        $item4->setDescription('pen apple');
-        $item4->setUnitPrice(13.75);
-
-
-        $items = [$item1, $item2, $item3, $item4];
         return $this->render('item/list.html.twig', [
             'items' => $items
         ]);
---
+++ src/Migrations/Version20200201092644.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200201092644 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE item_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE item (id INT NOT NULL, item_number VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, unit_price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE item_id_seq CASCADE');
+        $this->addSql('DROP TABLE item');
+    }
+}
