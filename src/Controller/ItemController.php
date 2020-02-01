f96441ecf9a53500788de641832f868918a0ce4b
pgolez <paul.golez@codingavenue.com>
2020-02-01 14:36:29 +0800

    Create controller for items

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Item;
 
 class ItemController extends AbstractController
 {
@@ -12,8 +13,45 @@ class ItemController extends AbstractController
      */
     public function index()
     {
+        $item1 = new Item();
+        $item1->setItemNumber('10120');
+        $item1->setDescription('pen');
+        $item1->setUnitPrice(12.00);
+
         return $this->render('item/index.html.twig', [
-            'controller_name' => 'ItemController',
+            'item'  => $item1
+        ]);
+    }
+
+    /**
+     * @Route("/items", name="list_items")
+     */
+    public function listItems()
+    {
+        $item1 = new Item();
+        $item1->setItemNumber('10120');
+        $item1->setDescription('pen');
+        $item1->setUnitPrice(12.00);
+
+        $item2 = new Item();
+        $item2->setItemNumber('101334');
+        $item2->setDescription('pen 2');
+        $item2->setUnitPrice(12.50);
+
+        $item3 = new Item();
+        $item3->setItemNumber('324234');
+        $item3->setDescription('expensive pen');
+        $item3->setUnitPrice(82.00);
+
+        $item4 = new Item();
+        $item4->setItemNumber('123123');
+        $item4->setDescription('pen apple');
+        $item4->setUnitPrice(13.75);
+
+
+        $items = [$item1, $item2, $item3, $item4];
+        return $this->render('item/list.html.twig', [
+            'items' => $items
         ]);
     }
 }
